1f0c30fa0c2207f3d3f43b51a90a690df31add57
masrimustaman <mado.masuri@gmail.com>
2020-09-15 23:36:52 +0800

    2nd upload. no 11-21

--- Generate-ServerReport.ps1
+++ Generate-ServerReport.ps1
@@ -12,9 +12,9 @@ $filename = "$(get-date -f yyyy-MM-dd)_$($env:COMPUTERNAME)_ServerReport.txt"
 "Basic Information" | Out-File $filename -Append
 "----------------------------------------------------------------------------"  | Out-File $filename -Append
 "$env:COMPUTERNAME Server Report" | Out-File $filename -Append
+"OS Version : $((Get-WmiObject -class Win32_OperatingSystem).Caption)" | Out-File $filename -Append
 "Date : $(get-date)" | Out-File $filename -Append
 "User : $env:USERNAME" | Out-File $filename -Append
-"" | Out-File $filename -Append
 "============================================================================"  | Out-File $filename -Append
 "Check Valid Server License" | Out-File $filename -Append
 "----------------------------------------------------------------------------"  | Out-File $filename -Append
@@ -150,3 +150,123 @@ Get-WmiObject Win32_logicaldisk | Format-Table SystemName, DeviceID, @{Name="Siz
 
 w32tm /query /peers | Out-File $filename -Append
 "============================================================================"  | Out-File $filename -Append
+"Check all services" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+
+gsv | select name, displayname, status, starttype | sort starttype, status | ft -AutoSize | Out-File $filename -Append
+"============================================================================"  | Out-File $filename -Append
+"Check Remote Desktop and IIS Services" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+
+gsv | where{$_.name -eq "W3SVC" -or $_.name -eq "TermService"} |select name, displayname, status, starttype | sort starttype, status | ft -AutoSize | Out-File $filename -Append
+"============================================================================"  | Out-File $filename -Append
+"Check Remote Desktop and IIS Services Failure Action" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+
+
+try { 
+Start-Process sc.exe -ArgumentList "failure W3SVC reset=0 actions=restart/60000/restart/60000/restart/60000"
+Start-Process sc.exe -ArgumentList "failure TermService reset=0 actions=restart/60000/restart/60000/restart/60000"
+"Successfully configured failure action to restart service on each failure Remote Desktop Services and IIS services"  | Out-File $filename -Append
+ }
+catch { "Error occured while setting failure action to Remote Desktop Services and IIS services"  | Out-File $filename -Append}
+
+"============================================================================"  | Out-File $filename -Append
+"Check Remote Desktop and IIS Required Services" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+
+gsv | where{$_.name -eq "W3SVC" -or $_.name -eq "TermService"} |select name, displayname, status, starttype, RequiredServices | sort starttype, status | ft -AutoSize | Out-File $filename -Append
+
+"============================================================================"  | Out-File $filename -Append
+"Ensure SSH is running and configured as automatic" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+gsv *sshd* |Set-Service -StartupType Automatic
+gsv *sshd* |select name, displayname, status, starttype| ft -AutoSize | Out-File $filename -Append
+"============================================================================"  | Out-File $filename -Append
+"Configuring Powershell as default shell for OpenSSH" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+Get-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -name defaultshell | Out-File $filename -Append
+"============================================================================"  | Out-File $filename -Append
+"Check the current SMB Share" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+Get-SmbShare | Out-File $filename -Append
+"============================================================================"  | Out-File $filename -Append
+"Checking Network Connectivity" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+netstat -aon | Out-File $filename -Append
+
+"============================================================================"  | Out-File $filename -Append
+"Remove Network HTTP/HTTPS Proxy" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+Function Disable-NetProxy
+{
+  Begin
+    {
+            $regKey="HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    }
+    Process
+    {
+        Set-ItemProperty -path $regKey ProxyEnable -value 0 -ErrorAction Stop
+        Set-ItemProperty -path $regKey ProxyServer -value "" -ErrorAction Stop
+        Set-ItemProperty -path $regKey AutoConfigURL -Value "" -ErrorAction Stop       
+    }
+   
+    End
+    {
+        "Proxy is now Disabled"
+
+    }
+}
+
+Disable-NetProxy  | Out-File $filename -Append
+
+"============================================================================"  | Out-File $filename -Append
+"Checking All Cert in Certificate Store" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+Get-ChildItem -Path cert: -Recurse | select @{ Name = 'Path';  Expression = {$_.pspath.replace("Microsoft.PowerShell.Security\Certificate::","")}}, FriendlyName, NotAfter | ft -AutoSize  | Out-File $filename -Append
+
+"============================================================================"  | Out-File $filename -Append
+"Checking IIS Configuration" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+"Ensure Web content is on a Non-System Partition" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+$test = Get-Content (Join-Path -Path $Env:SystemRoot -ChildPath 'System32\inetsrv\config\applicationHost.config')
+
+If (
+    (Test-Path -Path (Join-Path -Path $Env:SystemDrive -ChildPath 'inetpub')) -And
+    $test -Match [RegEx]::Escape((Join-Path -Path $Env:SystemDrive -ChildPath 'inetpub'))
+) {
+    $true | Out-File $filename -Append
+} Else {
+    $false| Out-File $filename -Append
+}
+"" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+
+"Ensure ‘unique application pools’ is set for sites"| Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+[Bool](Get-WebApplication | Group-Object -Property 'applicationPool' | Where-Object 'count' -GT 1)| Out-File $filename -Append
+"" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+
+"Ensure ‘application pool identity’ is configured for anonymous user identity"| Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+Get-ChildItem 'IIS:\Sites' | Foreach-Object {
+  $anonAuth = (Get-WebConfigurationProperty -Filter '/system.webServer/security/authentication/anonymousAuthentication' -PSPath "IIS:\Sites\$($_.Name)" -Name "userName").Value
+
+  [PSCustomObject]@{
+    "Name" = $_.Name
+    "AnonAuth" = $(If($anonAuth -EQ '') {$false}Else{$true})
+  }
+}| Out-File $filename -Append
+"" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+
+"Ensure WebDav feature is disabled"| Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+[Bool](Get-WindowsFeature -Name 'Web-DAV-Publishing' | Where-Object Installed -EQ $true)| Out-File $filename -Append
+"" | Out-File $filename -Append
+"----------------------------------------------------------------------------"  | Out-File $filename -Append
+
+
